82deed7c07d5f00b8dc92164e738d8ce6be856e2
Bruno Silva <bruno.sidf@gmail.com>
2019-03-29 18:00:19 +0000

    Testes

--- src/test/java/dev/codenation/gestaohospital/GestaohospitalApplicationTests.java
+++ src/test/java/dev/codenation/gestaohospital/GestaohospitalApplicationTests.java
@@ -1,16 +1,38 @@
 package dev.codenation.gestaohospital;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.client.RestClientException;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(SpringExtension.class)
+@AutoConfigureMockMvc
 public class GestaohospitalApplicationTests {
+	
+	private static final String BASE_PATH = "/v1/hospitais";
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Autowired
+    private TestRestTemplate restTemplate;
+	
+	@LocalServerPort
+    private int port;
 
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws RestClientException, URISyntaxException {
+		restTemplate.getForEntity(new URI("http://localhost:"+port+"/v1/hospitais").toString(), String.class);
 	}
 
 }
